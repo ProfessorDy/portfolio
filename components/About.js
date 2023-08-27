f46647ed48df4bf9db6ad8aa76afaa45c5bd5b2f
ProfessorDy <kingdeeyfx@gmail.com>
2023-08-27 15:04:52 +0100

    Create button component

--- components/About.js
+++ components/About.js
@@ -1,10 +1,23 @@
 import Image from "next/image"
+import Link from "next/link"
+import Button from "./Button"
 
 const About = () => {
     return (
         <section className="py-20 sm:py-24 lg:py-[9.375rem]">
-            <Image />
-            <article></article>
+            <div>
+                <Image />
+                <article className="text-grayish-dark-blue">
+                    <h2 className=" font-ibarra text-[2.5rem] font-bold leading-[2.625rem] tracing-[-0.2231rem]">About Me</h2>
+                    <p className="font-normal leading-[1.875rem opacity-80">I’m a junior front-end developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to whatever tools are required. I’m based in London, UK, but I’m happy working remotely and have experience in remote teams. When I’m not coding, you’ll find me outdoors. I love being out in nature whether that’s going for a walk, run or cycling. I’d love you to check out my work.</p>
+                    <Button route='/portfolio' text='go to portfolio' />
+                </article>
+            </div>
+            <div>
+                <h3>Interested in doing a project together?</h3>
+                <Link href='/contact'>contact me</Link>
+
+            </div>
         </section>
     )
 }
---
+++ components/Button.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+
+
+const Button = ({ route, text }) => {
+    return (
+        <Link href={route} className="inline-block bg-white hover:bg-grayish-dark-blue border hover:border-0 border-grayish-dark-blue py-4 px-8 text-grayish-dark-blue hover:text-very-light-grey text-[0.75rem] font-normal tracing-[0.125rem] uppercase">{text}</Link>
+    )
+}
+
+export default Button  
