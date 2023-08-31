2bf5f0dd7bb8e749af8dddd272569e33e945ce9e
ProfessorDy <kingdeeyfx@gmail.com>
2023-08-31 05:37:03 +0100

    Avatar

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,15 @@
+import Form from "@/components/Form";
+
 export default function Contact() {
     return (
         <main className=' min-h-screen bg-very-light-grey px-8 sm:px-10'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa rem necessitatibus totam magnam quibusdam deserunt voluptas vitae repudiandae minus error? Vero totam impedit maxime. Incidunt delectus reiciendis repellat sequi reprehenderit. Recusandae, iure veritatis. Doloremque!
+            <section>
+                <div>
+                    <h1>Get in Touch</h1>
+                    <article></article>
+                </div>
+            </section>
+            <Form />.
         </main>
     )
 }
